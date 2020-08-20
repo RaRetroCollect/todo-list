d443ffa3d53f1678102fdd5ec95a521f74c1fbe0
Ramon van Delft <r.vandelft@heemskerkflower.com>
2020-08-20 16:52:29 +0200

    Updates

---
+++ precache-manifest.ddce06d06527bfb816c8e50c81778303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e4aa8654d646e21d240082a2f2ca26",
+    "url": "/RaRetroCollect/todo-list/index.html"
+  },
+  {
+    "revision": "47e2e98a11ed38eea929",
+    "url": "/RaRetroCollect/todo-list/static/css/main.a8950f64.chunk.css"
+  },
+  {
+    "revision": "b570a59c7a8c70900f1b",
+    "url": "/RaRetroCollect/todo-list/static/js/2.faab6034.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RaRetroCollect/todo-list/static/js/2.faab6034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e2e98a11ed38eea929",
+    "url": "/RaRetroCollect/todo-list/static/js/main.a91a015d.chunk.js"
+  },
+  {
+    "revision": "d23bfc46e9392e99e210",
+    "url": "/RaRetroCollect/todo-list/static/js/runtime-main.6c6f739b.js"
+  }
+]);
