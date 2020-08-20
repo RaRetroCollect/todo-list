1a9c901dd69d78d98878f8cb8e1b7bc3c04615df
Ramon van Delft <r.vandelft@heemskerkflower.com>
2020-08-20 18:11:37 +0200

    Update InputTodo.js

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,4 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react"
+
+const InputTodo = props => {
+    const [inputText, setInputText] = useState({
+        title: "",
+    })
+
+    const onChange = e => {
+        setInputText({
+            ...inputText,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        props.addTodoProps(inputText.title)
+        setInputText({
+            title: "",
+        })
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <input
+                type="text"
+                className="input-text"
+                placeholder="Add todo..."
+                value={inputText.title}
+                name="title"
+                onChange={onChange}
+            />
+            <input type="submit" className="input-submit" value="Submit" />
+        </form>
+    )
+}
+
+export default InputTodo
+
+/*import React, { Component } from "react";
 
 class InputTodo extends Component {
     state = {
@@ -37,3 +76,4 @@ class InputTodo extends Component {
 }
 
 export default InputTodo
+*/
